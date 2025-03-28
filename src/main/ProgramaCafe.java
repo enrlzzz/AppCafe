d8c8cc5ae7d53ae27ad41ffcbbd8c7ad1422b902
Aluno <aluno@facens.br>
2025-03-27 21:33:48 -0300

    Primeiro 1

---
+++ src/main/ProgramaCafe.java
@@ -0,0 +1,10 @@
+package main;
+
+import controller.CafeController;
+public class ProgramaCafe {
+
+    public static void main(String[] args) {
+        CafeController controller = new CafeController();
+        controller.iniciarPreparoCafe();
+    }
+}
--- src/model/ItensCafe.java
+++ src/model/ItensCafe.java
@@ -1,5 +1,25 @@
 package model;
 
 public class ItensCafe {
+
+    private boolean poCafe;
+    private boolean agua;
+    private boolean fogo;
+    private boolean coador;
+    private boolean bule;
+
+    public ItensCafe(Boolean poCafe, boolean agua, boolean fogo, boolean coador, boolean bule) {
+        this.poCafe = poCafe;
+        this.poCafe = agua;
+        this.poCafe = fogo;
+        this.poCafe = coador;
+        this.poCafe = bule;
+    }
+
+    public boolean verificaItens() {
+        return poCafe && agua && fogo && coador && bule;
+        
+    }
+
     
 }
